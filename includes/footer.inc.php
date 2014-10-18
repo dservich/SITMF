133b862e730792484158d240a1b0518a963c3250
Dan Servich <dservich@gmail.com>
2014-10-18 18:39:55 -0400

    changes to footer

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,13 +1,3 @@
-    </div> <!-- /container -->
 
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/questionResults.js"></script>
-	<script src="js/flot/jquery.flot.js"></script>
-    <script src="js/flot/jquery.flot.pie.js"></script>
   </body>
 </html>
