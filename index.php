a29d63a50d546317f25cde8e110e80410630fc50
Dan Servich <dservich@gmail.com>
2014-10-19 02:13:11 -0400

    repeat json

--- index.php
+++ index.php
@@ -148,6 +148,23 @@ require_once('includes/header.inc.php');
     
     
   </div>
+
+<!-- Practice -->
+
+  <div class = "row">
+  	 <?php
+  	 	$json = file_get_contents('http://api.nytimes.com/svc/news/v3/content/all/World/24.json?limit=1&offset=0&api-key=2cf1abc4b8adb0ad4a723a3b6e666c2a%3A2%3A70025254');
+  	 	$obj = json_decode($json, true);
+  	 	
+  	 	$article = $obj['results'][0];
+  	 	$url = $article['url'];
+
+  	 ?>
+  	 	<a href="<?php  echo $url?>"><img src = "http://placehold.it/300x300.png"/></a>
+  	 <?php
+
+  	?>
+  </div>
     
   
    
