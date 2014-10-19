0b56fb721ebf7231251310ef2005315c28d3c681
Dan Servich <dservich@gmail.com>
2014-10-19 01:10:34 -0400

    php stufffff for articles on homepage

--- index.php
+++ index.php
@@ -149,6 +149,15 @@ require_once('includes/header.inc.php');
   </div>
     
   
+
+  <div class = "row">
+  	 <?php
+  	 	$json = file_get_contents('http://api.nytimes.com/svc/news/v3/content/all/World/24.json?limit=1&offset=0&api-key=2cf1abc4b8adb0ad4a723a3b6e666c2a%3A2%3A70025254');
+  	 	$obj = json_decode($json, true);
+  	 	print_r($obj['results']);
+
+  	 ?>
+  </div>
    
   
   <footer class="row">
