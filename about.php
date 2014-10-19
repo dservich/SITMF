9649aa2ed5ebcc4b410e04b2f8e281917ebfe308
skilp <sms1030@sru.edu>
2014-10-18 23:29:20 -0400

    About

--- about.php
+++ about.php
@@ -1,7 +1,37 @@
 <?php
 require_once('includes/header.inc.php');
 ?>
+<body>
 
+
+
+
+<!-- This row is our navigation bar. -->
+<div class="row" >
+    <div class="large-3 columns">
+      <h1><img src="favicon.png" alt="logo" 
+      	onclick="window.open('index.php');"/>
+      
+      </h1>
+    </div>
+    <div class="large-9 columns">
+      <ul class="inline-list right" >
+        <li ><a href="#">About SITMF</a></li>
+        <li ><a href="contact.php">Contact Us</a></li>
+      </ul>
+    </div>
+  </div>
+
+<p>
+What is SITMF, you ask? SITMF is a site dedicated to providing its users
+ with news, and a way for them to voice their opinions through video about
+  it. Users can also view fellow user’s opinions to try and see different 
+  stances on topics. Not sure what your stance is on a certain subject? 
+  Simply type in a keyword and see the subject through someone else’s point
+   of view. Why read someone’s point of view written from behind a keyboard
+    when you can have them say it to your face? 
+</p>
+</body>
 
 
 <?php
