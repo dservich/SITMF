3d91cf8d6fcfe92b16dc3d04571473ee7831d6c8
Dan Servich <dservich@gmail.com>
2014-10-19 02:51:20 -0400

    stuff

--- index.php
+++ index.php
@@ -62,8 +62,20 @@ require_once('includes/header.inc.php');
      
  
         <div class="large-3 small-6 columns" id = "WorldThumbnail">
-          <img src="http://placehold.it/250x250&text=Thumbnail-1"/>
-          <h6 class="panel"><a href = "article.php">Click here for more news in World</a></h6>
+          <?php
+  	 		$json = file_get_contents('http://api.nytimes.com/svc/news/v3/content/all/World/24.json?limit=1&offset=0&api-key=2cf1abc4b8adb0ad4a723a3b6e666c2a%3A2%3A70025254');
+  	 		$obj = json_decode($json, true);
+  	 	
+  	 		$article = $obj['results'][0];
+  	 		$url = $article['url'];
+	      ?>
+  	 	<a href="<?php  echo $url?>"><img src="world.png"/></a>
+  	 <?php
+
+  	?>
+
+          
+          <h6 class="panel"><a href = "section.php">Click here for more news in World</a></h6>
         </div>
  
         <div class="large-3 small-6 columns" id = "SportsThumbnail">
@@ -106,6 +118,12 @@ require_once('includes/header.inc.php');
     </div>
   </div><br>
  
+
+ <!-- ----------------------------
+
+ This is the second row of the body 
+
+ -------------------------------- -->
   <div class="row">
     <div class="large-12 columns">
       <div class="row">
@@ -151,6 +169,25 @@ require_once('includes/header.inc.php');
   </div>
 
 <!-- Practice -->
+
+
+<!--
+  <div class = "row">
+  	 <?php/*
+  	 	$json = file_get_contents('http://api.nytimes.com/svc/news/v3/content/all/World/24.json?limit=1&offset=0&api-key=2cf1abc4b8adb0ad4a723a3b6e666c2a%3A2%3A70025254');
+  	 	$obj = json_decode($json, true);
+  	 	
+  	 	$article = $obj['results'][0];
+  	 	$url = $article['url'];
+
+  	 ?>
+  	 	<a href="<?php  echo $url?>"><img src = "http://placehold.it/300x300.png"/></a>
+  	 <?php
+
+  	*/?>
+  </div>
+    
+  -->
 
    
   
