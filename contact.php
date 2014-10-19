b938615fcc689dbd2047b71776ea56c9211eb301
skilp <sms1030@sru.edu>
2014-10-19 01:58:27 -0400

    god

--- contact.php
+++ contact.php
@@ -1,7 +1,60 @@
 <?php
 require_once('includes/header.inc.php');
 ?>
+<p>
 
+<!-- This row is our navigation bar. -->
+<div class="row" >
+    <div class="large-3 columns">
+      <h1><a href="index.php"/><img src="favicon.png" alt="logo"></a>
+      
+      </h1>
+    </div>
+    <div class="large-9 columns">
+      <ul class="inline-list right" >
+        
+        <li ><a href="about.php">About SITMF</a></li>
+      </ul>
+    </div>
+  </div>
+
+
+<center>		
+ <font size="16" color="#FF8000">Shane Skillpa</font> 
+<br>
+<font size="12">
+	<a href="mailto:sms1030@sru.edu">sms1030@sru.edu </a>
+<a href="https://twitter.com/Two_Shanez"/><img src="twitter.png" 
+width="50" height="50" alt="twitlogo">
+</a>
+<br>
+<br>
+<font color=#FF8000">Dan Servich</font>
+<br>
+<a href="mailto:dos5385@psu.edu"/>dos5385@psu.edu</a>
+<a href="https://www.facebook.com/dan.servich?fref=ts"/>
+<img src="facebook.png" width="50" height="50" alt="facebooklogo">
+</a>
+
+</font>
+</center>
+</p>
+<br>
+<br>
+<br>
+<br>
+
+<footer class="row">
+    <div class="large-12 columns">
+      <hr/>
+      <div class="row">
+        <div class="large-6 columns">
+          <p><b>© Dan Servich & Shane Skillpa.</b></p>
+        </div>
+      
+      </div>
+    </div> 
+  </footer>
 
 <?php
 	require_once('includes/header.inc.php');
