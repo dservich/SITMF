8304b44e700019f2ae95b0a7b91137bfc3f146c9
Dan Servich <dservich@gmail.com>
2014-10-19 05:15:57 -0400

    code of conduct

---
+++ code-of-conduct.php
@@ -0,0 +1,86 @@
+<?php
+require_once('includes/header.inc.php');
+?>
+
+<!-- This row is our title bar -->
+<div class = "row">
+	<div class = "large-12 columns">
+    <p align = "center" style="font-family:impact;
+     border-style:solid; border-color:black; background-color:#FFFF75" >
+ <font color="#FF8000" size="26">
+      <b><i>Say It To My Face</b></i></font> 
+		<br>
+    <font color="#FF0000" size="15">
+      <small>
+      <b><i>Where Your Opinion Is Heard</i></b></font>
+    </small>
+  </p>
+        
+    	
+	</div>
+</div>
+
+<!-- This row is our navigation bar. -->
+<div class="row" >
+    <div class="large-3 columns">
+      <h1><img src="favicon.png"/></h1>
+    </div>
+    <div class="large-9 columns">
+      <ul class="inline-list right" >
+        <li ><a href="about.php">About SITMF</a></li>
+        <li ><a href="contact.php">Contact Us</a></li>
+      </ul>
+    </div>
+  </div>
+  
+   <br>
+
+
+
+
+<!-- Body -->
+
+<div class = "row">
+	<div class = "large-12 columns">
+		<h3 align = "center">this is the code of conduct</h3>
+
+		<h6 align = "center"> Rules of appropriate engagement: <br>
+				<ol>
+					<li> No nudity </li>
+					<li> No threatening messages towards other individuals </li>
+					<li> No verbal/sexual harassment will be tolerated </li>
+				</ol>
+				<br>
+				We are not responsible for any illicit behavior performed through SITMF. Any violations will be punished by the fullest extent of the law.
+		</h6>
+	</div>
+</div>
+
+<!-- end Body -->
+
+
+
+<!-- Footer -->
+
+   <footer class="row">
+    <div class="large-12 columns">
+      <hr/>
+      <div class="row">
+        <div class="large-6 columns">
+          <p><b>© Dan Servich & Shane Skillpa.</b></p>
+        </div>
+        <div class="large-6 columns">
+          <ul class="inline-list right">
+            <li ><a href="about.php">About SITMF</a></li>
+          <li ><a href="contact.php">Contact Us</a></li>
+          </ul>
+        </div>
+      </div>
+    </div> 
+  </footer>
+
+
+
+<?php
+require_once('includes/footer.inc.php');
+?>
